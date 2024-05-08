dc7508b27b280879c661d0bc1463d71017d95a01
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-05-08 15:08:31 +0900

    試し

--- 弾幕ゲーム/src/smartphone.js
+++ 弾幕ゲーム/src/smartphone.js
@@ -1,7 +1,24 @@
 function isSmartPhone() {
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+      var smartPhoneData={
+        start:{x:0,y:0},
+        now:{x:0,y:0},
+        end:true,
+      }
       return true;
     } else {
       return false;
     }
 }
+
+document.addEventListener("touchstart",function (event) {
+    canvas.style.display='none';
+    smartPhoneData.end=false;
+    smartPhoneData.start={x:event.touches[0].pageX,y:event.touches[0].pageY}
+})
+document.addEventListener("touchmove", function(event) {
+    smartPhoneData.now={x:event.touches[0].pageX,y:event.touches[0].pageY}
+});
+document.addEventListener("touchend", function(event){
+    smartPhoneData.end=true
+})
