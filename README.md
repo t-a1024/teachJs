b0f706a6331878d6fa5e86ce4a5ce203cdb6cac7
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-05-09 15:14:10 +0900

    操作方法の追加

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # teachJs
+解説用ページはこちら
+https://t-a1024.github.io/teachJs/解説用ページ/
+デプロイ先はいじってない
--- 弾幕ゲーム/src/smartphone.js
+++ 弾幕ゲーム/src/smartphone.js
@@ -1,10 +1,7 @@
 function isSmartPhone() {
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-      var smartPhoneData={
-        start:{x:0,y:0},
-        now:{x:0,y:0},
-        end:true,
-      }
+      keySituation.Enter=true;
+      
       console.log("smartPhone");
       return true;
     } else {
@@ -12,16 +9,48 @@ function isSmartPhone() {
       return false;
     }
 }
-
+const smartPhoneData={
+start:{x:0,y:0},
+now:{x:0,y:0},
+end:true,
+}
+isSmartPhone();
 document.addEventListener("touchstart",function (event) {
-    canvas.style.display='none';
+  keySituation.d=true
     smartPhoneData.end=false;
-    smartPhoneData.start={x:event.touches[0].pageX,y:event.touches[0].pageY}
+    smartPhoneData.start={x:event.touches[0].clientX,y:event.touches[0].clientY}
 })
 document.addEventListener("touchmove", function(event) {
     smartPhoneData.now={x:event.touches[0].pageX,y:event.touches[0].pageY}
 });
+
 document.addEventListener("touchend", function(event){
     smartPhoneData.end=true
 })
-isSmartPhone();
+function AllKeyControl(tf) {
+    keySituation.a=tf;
+    keySituation.s=tf;
+    keySituation.d=tf;
+    keySituation.w=tf;
+    //keySituation.Enter=tf;
+}
+function controlBySmartPhone() {
+    if (smartPhoneData.end) {
+        AllKeyControl(false);
+    }else{
+      if (smartPhoneData.start.x+60<smartPhoneData.now.x) {
+          keySituation.d=true;
+          keySituation.a=false;
+      }else if(smartPhoneData.start.x-60>smartPhoneData.now.x){
+          keySituation.a=true;
+          keySituation.d=false;
+      }
+      if (smartPhoneData.start.y+60<smartPhoneData.now.y) {
+        keySituation.s=true;
+        keySituation.w=false;
+    }else if(smartPhoneData.start.y-60>smartPhoneData.now.y){
+        keySituation.w=true;
+        keySituation.s=false;
+    }
+    }
+}
